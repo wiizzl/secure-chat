f5659f5ce383b3f1170b0afe26aa23e0d6617e83
wiizzl <git@houlliere.com>
2025-12-09 20:48:40 +0100

    feat: update environment configuration and improve global styles

--- app/api/[[...slugs]]/route.ts
+++ app/api/[[...slugs]]/route.ts
@@ -1,17 +1,15 @@
 import { Elysia } from "elysia";
 
-import { env } from "@/lib/env";
 import { messages } from "./messages";
 import { rooms } from "./rooms";
 
 const app = new Elysia({ prefix: "/api" }).use(rooms).use(messages);
 
 type Api = typeof app;
-const apiUrl = env.NEXT_PUBLIC_APP_URL;
 
 const GET = app.fetch;
 const POST = app.fetch;
 const DELETE = app.fetch;
 
-export { apiUrl, DELETE, GET, POST };
+export { DELETE, GET, POST };
 export type { Api };
