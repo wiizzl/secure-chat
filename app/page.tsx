cccc2f1718e99ea7476042913744b113e19a7fed
wiizzl <git@houlliere.com>
2025-12-09 20:03:54 +0100

    feat: wrap RoomCreate component in Suspense for improved loading handling

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
 import { RoomCreate } from "@/components/room-create";
+import { Suspense } from "react";
 
 export default function HomePage() {
   return (
     <main className="flex min-h-screen flex-col justify-center items-center">
-      <RoomCreate />
+      <Suspense>
+        <RoomCreate />
+      </Suspense>
     </main>
   );
 }
