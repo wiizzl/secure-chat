f41b4370ffab2d980277c4928ac9e692d55b360c
Pierre H. <git@houlliere.com>
2026-04-19 13:06:53 +0200

    fix: api url

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -2,4 +2,4 @@ import { treaty } from "@elysiajs/eden";
 
 import type { Api } from "~/api/[[...slugs]]/route";
 
-export const api = treaty<Api>("http://localhost:3000").api;
+export const api = treaty<Api>("https://secure-chat-black.vercel.app").api;
